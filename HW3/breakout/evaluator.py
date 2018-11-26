cdfc39a0a60a76d911d86f1504c4bc0181212771
sarahpg <sarahpg@umich.edu>
2018-11-26 11:32:32 -0600

    changed evaluator to handle state_memory

--- HW3/breakout/evaluator.py
+++ HW3/breakout/evaluator.py
@@ -16,11 +16,13 @@ parser.add_argument('--hidden_size', default=200, help='Number of hidden neurons
 parser.add_argument('--learning_rate', default=0.0005, help='Learning rate for optimizer')
 parser.add_argument('--action_size', default=4, help='Number of actions in the game')
 parser.add_argument('--games', default=1000, help="Number of games to play")
+parser.add_argument('--memory_size', default=4, help='Number of memory frames stored per state')
 
 
 def main(args): 
     # set up and reset game environment 
     env = gym.make('Breakout-v0')
+    state_memory = deque(maxlen=args.memory_size)
     env.reset()
 
     # initialize networks
@@ -36,21 +38,32 @@ def main(args):
     # load game
     with tf.Session() as sess:
         print("SESSION STARTED")
-        saver.restore(sess, "model/model1200.ckpt")
+        saver.restore(sess, "model/model40.ckpt")
         print("MODEL RESTORED")
         for game in range(args.games):
-            # start game
-            state = preprocess(env.reset())
+            # start game & initialize memory
+            start_state = preprocess(env.reset())
+            for fill in range(args.memory_size):
+                state_memory.append(start_state)
+
             reward_total = 0
             while True: 
-                action = np.argmax(QNetwork.predict(sess, [state]))
+
+                action = np.argmax(QNetwork.predict(sess, state_memory))
                 new_state, reward, done, _ = env.step(action)
-                
+
+                # Save old_state_memory
+                old_state_memory = state_memory
+
+                # Add new_state to state_memory
+                state_memory.append(preprocess(new_state))
+
+                # Update reward total
+                reward_total = reward_total + reward
+
                 if done:
                     break
-                else:
-                    state = preprocess(new_state)
-                    reward_total = reward_total + reward
+
             print(reward_total)  
 
 if __name__ == "__main__":
