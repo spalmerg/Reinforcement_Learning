b80e8978ae33dc5796f6214c4272dd1170302606
sarahpg <sarahpg@umich.edu>
2018-11-27 16:09:35 -0600

    change evaluator

--- HW3/breakout/evaluator.py
+++ HW3/breakout/evaluator.py
@@ -13,11 +13,11 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 # Arguments
 parser = argparse.ArgumentParser(description='DQN for Breakout Game')
-parser.add_argument('--hidden_size', default=200, help='Number of hidden neurons in FC layers')
-parser.add_argument('--learning_rate', default=0.0002, help='Learning rate for optimizer')
+parser.add_argument('--hidden_size', default=512, help='Number of hidden neurons in FC layers')
+parser.add_argument('--learning_rate', default=0.00002, help='Learning rate for optimizer')
 parser.add_argument('--action_size', default=4, help='Number of actions in the game')
 parser.add_argument('--games', default=1000, help="Number of games to play")
-parser.add_argument('--memory_size', default=3, help='Number of memory frames stored per state')
+parser.add_argument('--memory_size', default=4, help='Number of memory frames stored per state')
 
 
 def main(args): 
